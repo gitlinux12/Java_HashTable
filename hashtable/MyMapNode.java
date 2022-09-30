7789482f738b04a322f9cf87bedad02e152553d9
gitlinux12 <mshirole94@gmail.com>
2022-09-30 15:24:52 +0530

    Check Frequncy of given Words

--- hashtable/MyMapNode.java
+++ hashtable/MyMapNode.java
@@ -5,12 +5,6 @@ public class MyMapNode<Key, Value> {
 	public Value value;
 	public MyMapNode<Key, Value> next;
 	
-	/**
-	 * Create My Map Node with key value pair data
-	 * 
-	 * @param key
-	 * @param value
-	 */
 	MyMapNode(Key key, Value value) {
 		this.key = key;
 		this.value = value;
